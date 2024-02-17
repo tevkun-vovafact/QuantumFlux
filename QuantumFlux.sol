f2434e8ebee2ab16eb79dd74c8d74f5edf966e40
Maudykaz <raleighalvey67@gmail.com>
2024-02-17 12:00:00 +0300

    Add version check function to contract

--- QuantumFlux.sol
+++ QuantumFlux.sol
@@ -1,16 +1,11 @@
+
 pragma solidity ^0.8.0;
 
 contract QuantumFlux {
-    mapping(address => uint256) public quanta;
+    string public version = "1.1";
 
-    event QuantumLeap(address indexed user, uint256 amount);
-
-    function leap(uint256 amount) public {
-        quanta[msg.sender] += amount;
-        emit QuantumLeap(msg.sender, amount);
-    }
-
-    function getQuanta(address user) public view returns (uint256) {
-        return quanta[user];
+    function getVersion() public view returns (string memory) {
+        return version;
     }
 }
+
