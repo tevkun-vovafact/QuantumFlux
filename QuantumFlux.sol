a0a09bb1e27b887ce5f44359e9f242bf4a866b48
tevkun-vovafact <michaelongaya@gmail.com>
2024-08-05 16:00:00 +0300

    Add resetState function to QuantumFlux.sol

--- QuantumFlux.sol
+++ QuantumFlux.sol
@@ -3,6 +3,10 @@ pragma solidity ^0.8.0;
 // Version bump to 0.8.17 for compatibility
 
 contract QuantumFlux {
+    function resetState() public {
+        currentState = 0;
+        emit StateUpdated(0, 0);
+    }
     string public version = "1.1";
 
     function getVersion() public view returns (string memory) {
